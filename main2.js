ff8b4355c5fded6ea3efaf7c2b0821cde2cc642c
razec2112 <razec.cz@gmail.com>
2023-04-17 20:27:42 -0400

    clase de abstraccion

--- main2.js
+++ main2.js
@@ -1,21 +1,73 @@
+class Classes {
+    constructor({
+        name,
+        teacher,
+    }) {
+        this.name = name;
+        this.teacher = teacher;
+    }
+}
+
+class Course {
+    constructor({
+        name,
+        classes = [],
+    }) {
+        this.name = name
+        this.classes = classes
+    }
+}
+
+const cursoProgBasica = new Course({
+    name: "Curso Gratis de Programación Básica",
+})
+
+const cursoDefinitivoHtmlCss = new Course({
+    name: "Curso Definitivo de HTML y CSS",
+})
+
+const cursoPracticoHtmlCss = new Course({
+    name: "Curso Práctico de HTML y CSS",
+})
+
+
 class learningPaths {
     constructor ({
-        id,
         name,
-        courses,
+        courses = [],
     }) {
-        this.id = id;
         this.name = name;
         this.courses =courses;
     }
 }
 
 const escuelaWeb = new learningPaths ({
-    id: 001,
+
     name: "Escuela Web",
     courses: [
-        "curso basico de JS",
-        "curso practico de JS"
+        cursoProgBasica,
+        cursoDefinitivoHtmlCss,
+        cursoPracticoHtmlCss,
+    ]
+})
+
+const escuelaData = new learningPaths ({
+
+    name: "Escuela de Data Sciense",
+    courses: [
+        cursoProgBasica,
+        "curso DataBusiness",
+        "curso DataViz"
+    ]
+})
+
+const escuelaVideojuagos = new learningPaths ({
+
+    name: "Escuela de Videojuegos",
+    courses: [
+        cursoProgBasica,
+        "curso Unreal",
+        "curso Unity"
     ]
 })
 
